48b4e0672fd5a408106f57b7d2bf4b42edd63825
DanieleSavoia <danielesavoia44@gmail.com>
2023-05-12 16:10:57 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+// Milestone 1
+//Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
+
+
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+// Milestone 5 - opzionale
+// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+// Consigli utili:
+// Si possono trascurare le scrollbar verticali, sia nel pannello dei messaggi, che nella lista dei contatti
+// I pulsanti e le icone possono non funzionare (a parte l’invio del messaggio)
+// Per gestire le date, può essere utile la libreria Luxon
+
+
+
+
+
 const app = Vue.createApp({
 	data() {
 		return {
+			activeIndex: 0,
 			contacts: [
 				{
 					name: 'Michele',
@@ -48,7 +74,7 @@ const app = Vue.createApp({
 							date: '20/03/2020 16:35:00',
 							message: 'Mi piacerebbe ma devo andare a fare la spesa.',
 							status: 'sent'
-						}
+						},
 					],
 				},
 				{
@@ -169,7 +195,6 @@ const app = Vue.createApp({
 					],
 				}
 			],
-			activeIndex: 0,
 		};
 	},
 	methods: {
@@ -177,15 +202,21 @@ const app = Vue.createApp({
 			const filteredArray = [];
 			// const filteredArray = .filter()
 			return filteredArray;
-		}
+		},
+		extractTimeFromDate(date) {
+			return date.split(' ')[1].slice(0, -3);
+		},
+		setActiveIndex(element) {
+			this.activeIndex = this.contacts.indexOf(element);
+
+		},
 	},
 	computed: {
 		filteredContacts() {
-			const filteredArray = [];
-			// const filteredArray = .filter()
-			return filteredArray;
-		}
-	}
+			// return this.contacts.filter(contact => ........);
+			return []; // TODO: sostituirlo con il valore corretto
+		},
+	},
 });
 
 app.mount('.app');
